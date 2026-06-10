6b7d2dba2d96477a75d107d348c663d99ffa26f0
AlexianeBA <barbealexiane07@gmail.com>
2026-06-10 10:33:58 +0200

    add docker-compose && correction of import in main

--- backend/crmevent/main.py
+++ backend/crmevent/main.py
@@ -1,5 +1,23 @@
 from fastapi import FastAPI
-from crmevent.routers import company, users, contact, opportunity, activity, event, quote
+from crmevent.routers import (
+    company as company_router,
+    users as users_router,
+    contact as contact_router,
+    opportunity as opportunity_router,
+    activity as activity_router,
+    event as event_router,
+    quote as quote_router,
+)
+from crmevent.db.base import Base
+from crmevent.db.session import engine
+
+from crmevent.models.company import Company
+from crmevent.models.users import Users
+from crmevent.models.contact import Contact
+from crmevent.models.opportunity import Opportunity
+from crmevent.models.activity import Activity
+from crmevent.models.event import Event
+from crmevent.models.quote import Quote
 
 app = FastAPI(
     title="CRM API",
@@ -8,14 +26,18 @@ app = FastAPI(
     docs_url="/docs",
 )
 
-app.include_router(company.router)
-app.include_router(users.router)
-app.include_router(contact.router)
-app.include_router(opportunity.router)
-app.include_router(activity.router)
-app.include_router(event.router)
-app.include_router(quote.router)
+app.include_router(company_router.router)
+app.include_router(users_router.router)
+app.include_router(contact_router.router)
+app.include_router(opportunity_router.router)
+app.include_router(activity_router.router)
+app.include_router(event_router.router)
+app.include_router(quote_router.router)
 
 @app.get("/")
 def root():
     return {"message": "Bienvenue dans le CRM API"}
+
+@app.on_event("startup")
+def on_startup():
+    Base.metadata.create_all(bind=engine)
